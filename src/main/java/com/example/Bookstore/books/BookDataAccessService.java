43d7261b70fed5f44f2107df5766b76b36db5748
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-07-14 14:26:11 +0200

    format code

--- src/main/java/com/example/Bookstore/books/BookDataAccessService.java
+++ src/main/java/com/example/Bookstore/books/BookDataAccessService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class BookDataAccessService implements BookDao{
+public class BookDataAccessService implements BookDao {
     BookRepository bookRepository;
 
     public BookDataAccessService(BookRepository bookRepository) {
