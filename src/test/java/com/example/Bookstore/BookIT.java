0852e1b0abdb8c88efb6afb21937ff166649c59d
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-07-30 20:59:37 +0200

    add Integration Test get all books

---
+++ src/test/java/com/example/Bookstore/BookIT.java
@@ -0,0 +1,29 @@
+package com.example.Bookstore;
+
+import com.example.Bookstore.books.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BookIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private static final String BOOK_PATH = "/api/v1/book";
+
+    @Test
+    void canGetBooks() {
+
+
+        // get all books
+        webTestClient.get()
+                .uri(BOOK_PATH)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+}
