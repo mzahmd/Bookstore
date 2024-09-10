3c0897e44f9b931cc379718561894733840b7d38
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-09-10 20:29:02 +0200

    removed comment

--- Frontend/src/services/client.ts
+++ Frontend/src/services/client.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const getCustomer = async () => {
   try {
     return await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/book`);
-    // return await axios.get(`http://localhost:8080/api/v1/book`);
   } catch (e) {
     console.log(e);
   }
