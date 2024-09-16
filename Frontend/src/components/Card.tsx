bed26d377ef2338ce4435a2c4858cdc72b047414
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-09-16 19:07:30 +0200

    delete book implemented

--- Frontend/src/components/Card.tsx
+++ Frontend/src/components/Card.tsx
@@ -7,6 +7,7 @@ import {
   Stack,
   Image,
 } from "@chakra-ui/react";
+import AlertDialogExample from "./DeleteBTN";
 
 const IMAGE =
   "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80";
@@ -78,6 +79,7 @@ export default function Card({ title, isbn, author }: ICard) {
               $199
             </Text> */}
           </Stack>
+          <AlertDialogExample isbn={isbn}/>
         </Stack>
       </Box>
     </Center>
--- Frontend/src/services/client.ts
+++ Frontend/src/services/client.ts
@@ -3,13 +3,21 @@ import { TBook } from "../entities/book";
 
 export const getBook = async () => {
   // try {
-    return await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/book`);
+  return await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/book`);
   // } catch (e) {
   //   console.log(e);
   // }
 };
 
 export const saveBook = async (book: TBook) => {
-    return await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/v1/book`, book);
+  return await axios.post(
+    `${import.meta.env.VITE_API_BASE_URL}/api/v1/book`,
+    book
+  );
+};
 
-}
+export const deleteBook = async (isbn: string) => {
+  return await axios.delete(
+    `${import.meta.env.VITE_API_BASE_URL}/api/v1/book/${isbn}`,
+  );
+};
