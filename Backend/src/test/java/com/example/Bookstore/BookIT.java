1dc31a85fcf8e2fa52f156881d336d31fa020246
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-10-21 15:57:29 +0200

    test config

--- Backend/src/test/java/com/example/Bookstore/BookIT.java
+++ Backend/src/test/java/com/example/Bookstore/BookIT.java
@@ -1,10 +1,15 @@
 package com.example.Bookstore;
 
+import com.example.Bookstore.customer.Customer;
+import com.example.Bookstore.customer.Gender;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookIT {
@@ -13,14 +18,33 @@ public class BookIT {
     private WebTestClient webTestClient;
 
     private static final String BOOK_PATH = "/api/v1/book";
+    private static final String CUSTOMER_PATH = "/api/v1/customer";
 
     @Test
     void canGetBooks() {
+
+        Customer register = new Customer("Max Mustermann", "max.mustermann@gmail.com", "password", 33, Gender.Male);
+
+        String jwtToken = webTestClient.post()
+                .uri(CUSTOMER_PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(register), Customer.class)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .returnResult(Void.class)
+                .getResponseHeaders()
+                .get(HttpHeaders.AUTHORIZATION)
+                .get(0);
+
+        System.out.println("Mein TOken: " + jwtToken);
 
 
         // get all books
         webTestClient.get()
                 .uri(BOOK_PATH)
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken))
                 .exchange()
                 .expectStatus()
                 .isOk();
