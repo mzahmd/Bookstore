d5cfeeafbcc920eca6fdfe001a3214342a009a5d
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-10-24 16:03:05 +0200

    add new Exception for authentication

--- Backend/src/main/java/com/example/Bookstore/exception/GlobalControllerExceptionHandler.java
+++ Backend/src/main/java/com/example/Bookstore/exception/GlobalControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.Bookstore.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,6 +23,14 @@ public class GlobalControllerExceptionHandler {
         ApiError error = new ApiError(e.getMessage(), HttpStatus.NOT_FOUND.value());
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    public ResponseEntity<ApiError> defaultException(InsufficientAuthenticationException e) {
+
+        ApiError error = new ApiError(e.getMessage(), HttpStatus.FORBIDDEN.value());
+
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(Exception.class)
