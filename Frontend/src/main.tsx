66ad062bb62873f0a574ee060c0f9b2ed86e0dec
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-10-29 20:13:17 +0100

    creating routing in frontend

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,14 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, createStandaloneToast } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 
 const { ToastContainer } = createStandaloneToast();
 
+const Login = () => <h1>Login page</h1>;
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+    // element: <h1>Login page</h1>
+    // element: () => <h1>Login page</h1>
+  },
+  {
+    path: "dashboard",
+    element: <App />
+  }
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router}/>
       <ToastContainer />
     </ChakraProvider>
   </StrictMode>
