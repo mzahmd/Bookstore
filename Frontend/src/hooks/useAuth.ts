4cf500d7bc3b9234ef491583cf5ad2df721f836e
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-11-24 21:42:00 +0100

    fixed folder structure

--- Frontend/src/hooks/useAuth.ts
+++ Frontend/src/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext } from "../components/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 
 export function useAuth() {
   const context = useContext(AuthContext);
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, createStandaloneToast } from "@chakra-ui/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AuthProvider from "./components/AuthContext.tsx";
+import AuthProvider from "./context/AuthContext.tsx";
 import App from "./App.tsx";
 import Login from "./components/Login.tsx";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
