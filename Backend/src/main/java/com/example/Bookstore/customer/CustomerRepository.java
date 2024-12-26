7efb88a582bea63ae01d5f1bfa7da97eee9d7241
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-12-26 21:38:09 +0100

    remove comment

--- Backend/src/main/java/com/example/Bookstore/customer/CustomerRepository.java
+++ Backend/src/main/java/com/example/Bookstore/customer/CustomerRepository.java
@@ -1,7 +1,6 @@
 package com.example.Bookstore.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
