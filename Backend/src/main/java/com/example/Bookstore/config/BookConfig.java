1d7db6c8f38b5ba508200d0e761292092376ff7b
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-12-26 11:05:31 +0100

    removed import

--- Backend/src/main/java/com/example/Bookstore/config/BookConfig.java
+++ Backend/src/main/java/com/example/Bookstore/config/BookConfig.java
@@ -10,8 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.UUID;
-
 @Configuration
 public class BookConfig {
 
