4b44d79a6d8065e1c201bd523d5c8b72ec18dcb4
mahme001 <minhaz.ahmed@student.hs-rm.de>
2025-02-08 19:42:57 +0100

    removed comments in Customer

--- Backend/src/main/java/com/example/Bookstore/customer/Customer.java
+++ Backend/src/main/java/com/example/Bookstore/customer/Customer.java
@@ -122,19 +122,16 @@ public class Customer implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-//        return UserDetails.super.isAccountNonExpired();
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-//        return UserDetails.super.isAccountNonLocked();
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-//        return UserDetails.super.isCredentialsNonExpired();
         return true;
     }
 
